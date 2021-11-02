16d3b483cb4cd47c1f639f987acbf2b8bc77fd55
br36b <br36b@mail.umsl.edu>
2021-11-02 17:21:23 -0500

    Moved output files to their own directories

--- 2-project/constants.py
+++ 2-project/constants.py
@@ -1,8 +1,9 @@
 # Constants
 SALT_LENGTH = 1
+SALT_FILE = "users/salts.txt"
 
 KEY_FILENAME = "crypt.key"
-PLAIN_TEXT_FILENAME = "data.txt"
-HASH_TEXT_FILENAME = "data.hash"
-HASH_SALT_TEXT_FILENAME = "data.hasalt"
+PLAIN_TEXT_FILENAME = "users/data.txt"
+HASH_TEXT_FILENAME = "users/data.hash"
+HASH_SALT_TEXT_FILENAME = "users/data.hasalt"
 
