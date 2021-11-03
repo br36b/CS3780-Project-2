026173315be67e379a5195ed687f9fb58d658320
br36b <br36b@mail.umsl.edu>
2021-11-03 10:11:43 -0500

    Condensed authentication loops. Disallowed duplicate users.

--- 2-project/main.py
+++ 2-project/main.py
@@ -5,8 +5,6 @@
 
 import random
 
-from cryptography.fernet import Fernet
-
 from crypt_manager import generate_hashed_key, is_correct_password
 
 from input_functions import *
@@ -16,23 +14,23 @@ from file_functions import *
 
 # Display menu of options
 def print_menu() -> ():
-    print(
-    '''
+    print('''
     Would you like to:
         1) Create an account
         2) Authenticate
-        3) Exit
+        3) Generate random accounts
+        0) Exit
     ''')
 
 
 # Function to get all lines from a file stored as an array
 # To be used to read key file and username/password text pairs
 # returns array with lines
-def file_lines_to_array(filename: str) -> []:
+def file_lines_to_users(filename: str) -> []:
     try:
         with open(filename, "r") as file:
             # 'readlines()' returns a list containing each line in the file as a list item
-            return file.readlines()
+            return [user.strip().split(":") for user in file.readlines()]
 
     except IOError:
         print("FLTA: File was unable to be opened for writing")
@@ -45,11 +43,13 @@ def file_lines_to_array(filename: str) -> []:
 def user_data_format(username: str, *args) -> str:
     base_string = username
 
+    # Unpack tuple containing (password_key, salt)
     for arg in args:
+        # Prevent empty/default salt from being output into array structure
         if not arg == b"":
             base_string += ":"
 
-            # Double check everything is decoded
+            # Double check everything is decoded to avoid concatenation errors
             if isinstance(arg, bytes):
                 base_string += arg.decode()
             else:
@@ -57,6 +57,29 @@ def user_data_format(username: str, *args) -> str:
 
     return base_string
 
+
+# Function to prevent duplicate usernames in files
+def check_if_user_exists(username: str) -> bool:
+    plain_users = file_lines_to_users(PLAIN_TEXT_FILENAME)
+    hashed_users = file_lines_to_users(HASH_TEXT_FILENAME)
+    salt_hash_users = file_lines_to_users(HASH_SALT_TEXT_FILENAME)
+
+    user_data = [plain_users, hashed_users, salt_hash_users]
+    user_data_strings = ["Plain-text", "Hashed", "Salt+Hashed"]
+
+    for data in user_data:
+
+        # Empty array, no matches
+        if not data:
+            return False
+
+        # Username | Key | Salt
+        for user in data:
+            # return once a matching username is found
+            if user[0] == username:
+                return True
+
+    return False
 
 # Function to generate the files requested
     # A plaintext username password pair, stored in text in a file
@@ -97,66 +120,67 @@ def create_account() -> ():
     username = validate_username()
     password = validate_password()
 
-    generate_files(username, password)
+    # Make sure username doesn't already have an entry
+    if not check_if_user_exists(username):
+        generate_files(username, password)
+    else:
+        print("Error: Account could not be created. Reason: Username is taken.")
 
 
 def authenticate_account() -> ():
     print("\nYou have chosen to authenticate your account.")
 
     # Reusing creation functions, same behavior
+    # Makes sure username and password rules are followed before proceeding
     username = validate_username()
     password = validate_password()
 
-    plain_users = [user.strip().split(":") for user in file_lines_to_array(PLAIN_TEXT_FILENAME)]
-    hashed_users = [user.strip().split(":") for user in file_lines_to_array(HASH_TEXT_FILENAME)]
-    salt_hash_users = [user.strip().split(":") for user in file_lines_to_array(HASH_SALT_TEXT_FILENAME)]
+    # Fetch an array version of all
+    plain_users = file_lines_to_users(PLAIN_TEXT_FILENAME)
+    hashed_users = file_lines_to_users(HASH_TEXT_FILENAME)
+    salt_hash_users = file_lines_to_users(HASH_SALT_TEXT_FILENAME)
 
     user_data = [plain_users, hashed_users, salt_hash_users]
+    user_data_strings = ["Plain-text", "Hashed", "Salt+Hashed"]
 
+    count = 0
     for data in user_data:
+        is_found = False
+
+        # Empty array meaning data failed to be compiled
         if not data:
-            print("Error: There were missing data files.")
+            print("Auth Error: There were missing data files for {}.".format(user_data_strings[count]))
             return
 
-    # Arrays constructed with formatted data
-    # Username | Key | Salt
-    # print(plain_users)
-    # print(hashed_users)
-    # print(salt_hash_users)
+        # Arrays constructed with formatted data
+        # Username | Key | Salt
+        # To reduce code length, each type of file will be iterated here
+        for user in data:
+            # Count variable used to modify password verification for each file type
+            # Once a matching username is found
+            if user[0] == username:
+                # Plain-text
+                if count == 0:
+                    is_found = user[1] == password
+                # Hashed
+                elif count == 1:
+                    is_found = is_correct_password(password, user[1])
+                # Salt + Hash
+                elif count == 2:
+                    is_found = is_correct_password(password, user[1], provided_salt=user[2])
 
-    is_found = False
+        # Output if a login was matched or not
+        if is_found:
+            print("{}: Account was found. Login successful.".format(user_data_strings[count]))
+        else:
+            print("{}: No account was found with that information.".format(user_data_strings[count]))
 
-    for user in plain_users:
-        if user[0] == username:
-            # Unencrypted can be directly checked
-            is_found = user[1] == password
+        # Increment count to allow hash/hash+salt
+        count += 1
 
-    if is_found:
-        print("Plain-text: Account was found. Login successful.")
-    else:
-        print("Plain-text: No account was found with that login information.")
 
-    is_found = False
-    for user in hashed_users:
-        if user[0] == username:
-            # Must pass to function to verify hashes
-            is_found = is_correct_password(password, user[1])
-
-    if is_found:
-        print("Hash: Account was found. Login Successful.")
-    else:
-        print("Hash: No account was found with that login information.")
-
-    is_found = False
-    for user in salt_hash_users:
-        if user[0] == username:
-            # Must attach salt for hash+salt
-            is_found = is_correct_password(password, user[1], provided_salt=user[2])
-
-    if is_found:
-        print("Hash+Salt: Account was found. Login successful.")
-    else:
-        print("Hash+Salt: No account was found with that login information.")
+def generate_random_accounts() -> ():
+    print("Generate Random Accounts")
 
 
 # Main Loop
@@ -166,13 +190,15 @@ def main() -> ():
     while is_running:
         # Prompt for user option, 1-3
         print_menu()
-        menu_option = get_valid_integer("Please select a menu option", 1, 3)
+        menu_option = get_valid_integer("Please select a menu option", 0, 3)
 
         if menu_option == 1:  # Option 1 is to create an account
             create_account()
         elif menu_option == 2:  # Option 2 is to authenticate an account
             authenticate_account()
-        elif menu_option == 3:  # Option 3 is to exit
+        elif menu_option == 3:
+            generate_random_accounts()
+        elif menu_option == 0:  # Option 0 is to exit
             print("Exiting...")
             is_running = False
         else:  # Input for menu should be locked and this line should not be reached
