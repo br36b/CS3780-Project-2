0cad22cd315d6d65de5b2b1cfc2e9171c0f16624
br36b <br36b@mail.umsl.edu>
2021-11-06 10:56:42 -0500

    Updated to create file if missing

--- 2-project/file_functions.py
+++ 2-project/file_functions.py
@@ -1,11 +1,11 @@
 def write_to_file(filename: str, data: str) -> ():
     """
     Function to write string to file
-    To be used to write user data files
+    To be used to write user data files, will create one if it doesn't exist
     Data should not be modified here, just written
     """
     try:
-        with open(filename, "a") as file:
+        with open(filename, "a+") as file:
             file.write("{}\n".format(data))
 
     except IOError:
