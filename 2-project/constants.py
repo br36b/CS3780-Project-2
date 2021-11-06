21875b3e7f71a5820fbabbb22be90b4a9051c79c
br36b <br36b@mail.umsl.edu>
2021-11-06 10:26:07 -0500

    Moved salt characters to constants to be reused

--- 2-project/constants.py
+++ 2-project/constants.py
@@ -1,7 +1,11 @@
+from string import ascii_lowercase
 # Constants
 SALT_LENGTH = 1
 HASH_LENGTH = 32
 HASH_ITERATIONS = 1
+
+SALT_CHARS = list(ascii_lowercase)
+SALT_CHARS.extend([str(num) for num in range(0, 10)])
 
 # Output file names
 PLAIN_TEXT_FILENAME = "users/data.txt"
